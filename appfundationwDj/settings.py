37631677c3735ba77e4adfb7334dc0c1380244c7
Thomas Cornejo <cuc20131002@mail.udes.edu.co>
2025-10-10 00:20:27 -0500

    feat: add History model and HistoryInline for animal records

--- appfundationwDj/settings.py
+++ appfundationwDj/settings.py
@@ -128,6 +128,8 @@ USE_TZ = True
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 

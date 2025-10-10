84c0286eb1ae4efbe8fd56063dfbc3469dc5a6d8
Thomas Cornejo <cuc20131002@mail.udes.edu.co>
2025-10-10 00:22:58 -0500

    fix: delete import date in admin of animals

--- animals/admin.py
+++ animals/admin.py
@@ -1,7 +1,6 @@
 from django.utils.html import format_html
 from django.contrib import admin
 from .models import Animal, History
-from datetime import date
 
 
 class HistoryInline(admin.StackedInline):

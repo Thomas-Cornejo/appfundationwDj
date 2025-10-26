425f03033dab5b2d5d4d11508674beb97b4cb71a
Thomas Cornejo <cuc20131002@mail.udes.edu.co>
2025-10-26 18:46:38 -0500

    feat: add adoption and animal detail templates, with views logical for filters in templates and fix templates users

--- animals/urls.py
+++ animals/urls.py
@@ -1,3 +1,7 @@
+from django.urls import path
+from . import views
+
 urlpatterns = [
-    
+    path("adopt", views.animal_list, name="adoption_list"),
+    path("<int:animal_id>/", views.animal_detail, name="animal_detail"),
 ]
--- animals/views.py
+++ animals/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
+from .models import Animal, Breed
 
-# Create your views here.
+
+def animal_list(request):
+    breeds = Breed.objects.all()
+
+    selected_breed = request.GET.get('breed')
+    selected_birth_date = request.GET.get('birth_date')
+    selected_size = request.GET.get('size')
+
+    animals = Animal.objects.all()
+    
+    if selected_breed:
+        animals = animals.filter(breed_id=selected_breed)
+
+    if selected_birth_date:
+        if selected_birth_date == "joven":
+            animals = animals.filter(age__lte=2)
+        elif selected_birth_date == "adulto":
+            animals = animals.filter(age__gte=3, age__lte=7)
+        elif selected_birth_date == "senior":
+            animals = animals.filter(age__gte=8)
+
+    if selected_size:
+        animals = animals.filter(size=selected_size)
+
+    paginator = Paginator(animals, 8) 
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        "breeds": breeds,
+        "page_obj": page_obj,
+        "selected_breed": selected_breed or "",
+        "selected_birth_date": selected_birth_date or "",
+        "selected_size": selected_size or "",
+    }
+
+    return render(request, "animals/adoption.html", context)
+
+
+def animal_detail(request, animal_id):
+    """
+    Vista de detalle de un animal específico.
+    """
+    animal = get_object_or_404(Animal, id=animal_id)
+
+    context = {
+        "animal": animal
+    }
+
+    return render(request, "animals/animal_detail.html", context)

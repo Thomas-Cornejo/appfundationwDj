3ef188bf14300af611064fd70f7cdcd80b8654a8
Thomas Cornejo <cuc20131002@mail.udes.edu.co>
2025-10-31 15:34:21 -0500

    feat: Add initial configuration of app URLs, engagements, and variables for email sending

--- appfundationwDj/urls.py
+++ appfundationwDj/urls.py
@@ -17,9 +17,15 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include("users.urls")),
     path("animals/", include("animals.urls")),
+    path('engagements/', include('engagements.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

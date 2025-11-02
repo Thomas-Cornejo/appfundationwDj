b2b01fb7e08f53e3e418055bfbd61d643117d099
Thomas Cornejo <cuc20131002@mail.udes.edu.co>
2025-11-02 00:14:28 -0500

    feat: The user model was modified, fields were added due to the implementation of Shelter, and some user verifications were added. An admin role was added to manage users, assign groups, and perform other functions.

--- users/models.py
+++ users/models.py
@@ -1,9 +1,38 @@
 from django.db import models
+from shelters.models import Shelter
+
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     address = models.CharField(max_length=255, blank=True, null=True)
     phone = models.CharField(max_length=20, blank=True, null=True)
+    shelter = models.ForeignKey(Shelter, on_delete=models.SET_NULL, null=True, blank=True, related_name='staff_members', verbose_name='Albergue asignado')
+    experience_points = models.SmallIntegerField(default=0, verbose_name="Puntos de experiencia")
+    #rank = models.ForeignKey(Rank, on_delete=models.SET_NULL, null=True, blank=True, related_name=True, verbose_name="Rango actual")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.username}"
+    
+    def is_superadmin(self):
+        """Check if they are a super administrator"""
+        return self.is_superuser or self.groups.filter(name='Super Admin').exists()
+    def is_shelter_admin(self):
+        """Check if they are a hostel manager"""
+        return self.groups.filter(name='Shelter Admin').exists()
+    def is_regular_user(self):
+        """Check if they are a regular user (they can adopt/sponsor)"""
+        return self.groups.filter(name='Regular User').exists() or not self.groups.exists()
+    def can_manage_shelter(self, shelter):
+        """Check if you can manage a specific hostel"""
+        if self.is_superadmin():
+            return True
+        if self.is_shelter_admin() and self.shelter == shelter:
+            return True
+        return False
+    def get_managed_shelter(self):
+        """Return the shelter you manage (if you are shelter admin)"""
+        if self.is_shelter_admin():
+            return self.shelter
+        return None
